03cdbfafcc9435c237e9a60c02dcb4f6637c0dd8
Oleg <oleg@MacBook-Pro-Oleg.local>
2025-08-04 18:39:18 +0300

    сделан код и структура)

---
+++ bot_config.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+import os
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    bot_token: str = os.getenv("BOT_TOKEN")
+    ton_api_key: str | None = os.getenv("TON_API_KEY")
+    tron_api_key: str | None = os.getenv("TRON_API_KEY")
+    database_url: str = os.getenv("DATABASE_URL")
+    redis_url: str | None = os.getenv("REDIS_URL")
+    admin_password: str = os.getenv("ADMIN_PASSWORD")
+    channel_id: str | None = os.getenv("CHANNEL_ID")
+    base_webapp_url: str = os.getenv("BASE_WEBAPP_URL")
+
+
+settings = Settings()
