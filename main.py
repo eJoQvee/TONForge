4a95b2b06b34f96a17940916282b7e4b728c6717
Oleg <oleg@MacBook-Pro-Oleg.local>
2025-08-17 22:22:11 +0300

    описание изменений

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from handlers import router  # в handlers/__init__.py должен быть router
 
 async def main():
-    bot = Bot(os.environ["TELEGRAM_TOKEN"])
+    bot = Bot(os.environ["BOT_TOKEN"])
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
